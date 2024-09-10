034c82f6ffc6678c6b241eb74c770f3cb052a863
eliud1013 <eliud1013@Someone>
2024-09-09 21:24:22 -0700

    GP Fix

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725940043',resources: [
+    cacheName: 'chirpy-1725942237',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
