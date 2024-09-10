ead21208eb78f1647b3e57098ac75eb951874fa3
eliud1013 <eliud1013@Someone>
2024-09-09 20:50:23 -0700

    Sauna Writeup added

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725665540',resources: [
+    cacheName: 'chirpy-1725940043',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
