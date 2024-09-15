3bc7840c217e6491472bc32e26a0d67b9e3a48d7
eliud1013 <eliud1013@Someone>
2024-09-14 19:57:23 -0700

    Fixed terminal message

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1726368421',resources: [
+    cacheName: 'chirpy-1726368997',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
