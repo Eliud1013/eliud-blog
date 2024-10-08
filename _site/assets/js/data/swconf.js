557250641bfa641eacb052c061d3184ee2d5b6a6
eliud1013 <eliud1013@Someone>
2024-10-08 07:47:59 -0700

    Fixed cloud effect

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728396633',resources: [
+    cacheName: 'chirpy-1728398782',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
